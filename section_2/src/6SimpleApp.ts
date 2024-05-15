24d27ff6535ffbaea06336f5ae43fa28699dae97
mudriyjo <mudriyjo@gmail.com>
2024-05-16 01:17:08 +0400

    prepare homework description

---
+++ section_2/src/6SimpleApp.ts
@@ -0,0 +1,16 @@
+export {}
+
+/**
+ * Practices types by creating a simple app
+ * An employee manager app
+ * @Requrements
+ * - Create an Employee type
+ * - Create Employee with a function that asignees random ids
+ * - Send all employees an email (write to console)
+ * 
+ * Steps:
+ * 1. node project init
+ * 2. type install
+ * 3. create and run files
+ * 4. generate d.ts files
+ */
