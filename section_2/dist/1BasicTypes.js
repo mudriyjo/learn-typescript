7e80d2d13accf84174fa1ba2625feb99827289c7
mudriyjo <mudriyjo@gmail.com>
2024-05-16 20:56:58 +0400

    finish homework for lesson 1

--- section_2/dist/1BasicTypes.js
+++ section_2/dist/1BasicTypes.js
@@ -11,6 +11,6 @@ let work = () => {
 let salary = 50n;
 let logo = Symbol("emerald");
 let all = [firstName, age, isAdmin, duties, car, bicycle, work, salary, logo];
-for (let item in all) {
-    console.log(`${item}, type is: ${typeof item}`);
+for (let item of all) {
+    console.log(`${item?.toString()}, type is: ${typeof item}`);
 }
--- section_2/src/6SimpleApp.ts
+++ section_2/src/6SimpleApp.ts
@@ -1,4 +1,5 @@
-export {}
+export {Employee, createEmployee}
+import {randomBytes} from "crypto"
 
 /**
  * Practices types by creating a simple app
@@ -9,8 +10,40 @@ export {}
  * - Send all employees an email (write to console)
  * 
  * Steps:
- * 1. node project init
- * 2. type install
- * 3. create and run files
- * 4. generate d.ts files
+ * + 1. node project init
+ * + 2. type install
+ * + 3. create and run files
+ * + 4. generate d.ts files
  */
+type Employee = {
+    name: string
+    id: string
+    email: string
+    salary: number
+}
+
+function generateRandomId(): string {
+    return randomBytes(10).toString("hex")
+}
+
+function createEmployee(name: string, salary: number): Employee {
+    return {
+        name: name,
+        id: generateRandomId(),
+        email: `${name}@testCompany.com`,
+        salary: salary
+    }
+}
+function sendEmail(employee: Employee) {
+    console.log(`
+    Greetings in your new job, ${employee.name}!
+    Your corporate email is: ${employee.email}
+    Your salary is: ${employee.salary}
+    `)
+}
+
+let employees = [createEmployee("Alex", 200000), createEmployee("John", 300000)]
+
+employees.forEach(employe => {
+    sendEmail(employe)
+})
